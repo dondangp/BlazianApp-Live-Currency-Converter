47640d9dd83610dd546eccc32b3d76c48f610fc3
TahMeat <ron.nguyen@mavs.uta.edu>
2022-10-27 16:28:50 -0500

    test gson commit
    Signed-off-by: TahMeat <ron.nguyen@mavs.uta.edu>

--- app/src/main/java/com/example/BlazianApp/ConvertFragment.java
+++ app/src/main/java/com/example/BlazianApp/ConvertFragment.java
@@ -1,64 +1,83 @@
 package com.example.BlazianApp;
 
+
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.health.SystemHealthManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ConvertFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.BlazianApp.databinding.FragmentConvertBinding;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class ConvertFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ConvertFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ConvertFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ConvertFragment newInstance(String param1, String param2) {
-        ConvertFragment fragment = new ConvertFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private FragmentConvertBinding binding;
+    private static String BASE_URL = "https://v6.exchangerate-api.com/v6/c23a1af81c0b35138e9d1190/latest/";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //binding for user interface
+        binding = FragmentConvertBinding.inflate(inflater,container,false);
+
+        // Making Request and Get Rates+Currencies
+        new getAPI().execute();
+
+        // get multipliers
+        //String userCurrency = binding.userSpinner.getSelectedItem().toString();
+        //String userSelected = binding.convertSpinner.getSelectedItem().toString();
+
         return inflater.inflate(R.layout.fragment_convert, container, false);
+    }
+
+    public class getAPI extends AsyncTask<Void, Void, Void> {
+        String result;
+
+        @Override
+        protected Void doInBackground(Void... voids){
+            URL url = null;
+            try {
+                url = new URL(BASE_URL + "USD");
+                HttpURLConnection request = (HttpURLConnection) url.openConnection();
+                request.connect();
+
+                // Convert to JSON
+                Object results = request.getContent();
+                JsonObject jsonObject = JsonParser.parseReader(new InputStreamReader((InputStream) results)).getAsJsonObject();
+
+                // Accessing object
+                JsonObject conversionRates = jsonObject.get("conversion_rates").getAsJsonObject();
+                // Spinner Array
+                //List<String> spinnerArray =  new ArrayList<String>();
+                //spinnerArray = (List<String>) conversionRates.keySet();
+            } catch (MalformedURLException e) { e.printStackTrace(); }
+            catch (IOException e) { e.printStackTrace(); }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+        }
     }
 }
