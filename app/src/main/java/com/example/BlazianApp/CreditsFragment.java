ff1765a7a3f69898f50e4ffdb6cc9833553973df
TahMeat <ron.nguyen@mavs.uta.edu>
2022-11-12 18:02:50 -0600

    proper licensing and crediting.
    Signed-off-by: TahMeat <ron.nguyen@mavs.uta.edu>

--- app/src/main/java/com/example/BlazianApp/CreditsFragment.java
+++ app/src/main/java/com/example/BlazianApp/CreditsFragment.java
@@ -1,63 +1,35 @@
 package com.example.BlazianApp;
 
+
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CreditsFragment#newInstance} factory method to
- * create an instance of this fragment.
- *
- */
+
+import com.example.BlazianApp.databinding.FragmentCreditsBinding;
+
+
 public class CreditsFragment extends Fragment {
+    private FragmentCreditsBinding binding;
+    private WebView webView;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CreditsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CreditsFragment newInstance(String param1, String param2) {
-        CreditsFragment fragment = new CreditsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    public CreditsFragment() {
-        // Required empty public constructor
-    }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public void onCreate(Bundle savedInstanceState) { super.onCreate(savedInstanceState); }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_credits, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // bindings
+        binding = FragmentCreditsBinding.inflate(getLayoutInflater());
+        webView = binding.WebCredits;
+
+        // get html
+        String content = getString(R.string.credits_txt);
+        webView.loadDataWithBaseURL(null, content, "text/html", "utf-8", null);
+
+        return binding.getRoot();
     }
 }
