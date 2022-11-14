c26491d85af96b78d77d3eefe04c94307d183de3
TahMeat <ron.nguyen@mavs.uta.edu>
2022-11-13 18:52:06 -0600

    Started RecordFragment, editing UI for stuff.
    Signed-off-by: TahMeat <ron.nguyen@mavs.uta.edu>

--- app/src/main/java/com/example/BlazianApp/RecordFragment.java
+++ app/src/main/java/com/example/BlazianApp/RecordFragment.java
@@ -1,5 +1,6 @@
 package com.example.BlazianApp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,58 +8,35 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RecordFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.BlazianApp.databinding.FragmentRecordBinding;
+
 public class RecordFragment extends Fragment {
+    private FragmentRecordBinding binding;
+    private Button addRecord;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public RecordFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RecordFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RecordFragment newInstance(String param1, String param2) {
-        RecordFragment fragment = new RecordFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public void onCreate(Bundle savedInstanceState) { super.onCreate(savedInstanceState); }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_record, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //bindings
+        binding = FragmentRecordBinding.inflate(getLayoutInflater());
+        //initialize
+        addRecord = binding.newRecord;
+        String[] transactionRecords;
+
+        //Button Action
+        addRecord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), AddRecordActivity.class));
+            }
+        });
+
+
+        return binding.getRoot();
     }
 }
---
+++ app/src/main/java/com/example/BlazianApp/TransactionModel.java
@@ -0,0 +1,47 @@
+package com.example.BlazianApp;
+
+public class TransactionModel {
+    String transactionDate;
+    String transactionLocation;
+    String fromCurrency;
+    String toCurrency;
+    String fromFlag;
+    String toFlag;
+
+    public TransactionModel(String transactionDate,
+                            String transactionLocation,
+                            String fromCurrency,
+                            String toCurrency,
+                            String fromFlag, String toFlag) {
+        this.transactionDate = transactionDate;
+        this.transactionLocation = transactionLocation;
+        this.fromCurrency = fromCurrency;
+        this.toCurrency = toCurrency;
+        this.fromFlag = fromFlag;
+        this.toFlag = toFlag;
+    }
+
+    public String getTransactionDate() {
+        return transactionDate;
+    }
+
+    public String getTransactionLocation() {
+        return transactionLocation;
+    }
+
+    public String getFromCurrency() {
+        return fromCurrency;
+    }
+
+    public String getToCurrency() {
+        return toCurrency;
+    }
+
+    public String getFromFlag() {
+        return fromFlag;
+    }
+
+    public String getToFlag() {
+        return toFlag;
+    }
+}
