64ffb0e78e44c2ff1404b1782805c321a128d116
Megumi0843 <mxs4454@mavs.uta.edu>
2022-11-23 16:37:44 -0600

    a

---
+++ app/src/main/java/com/example/BlazianApp/Splash.java
@@ -0,0 +1,42 @@
+package com.example.BlazianApp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    String pin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        SharedPreferences settings = getSharedPreferences("PRESS",0);
+        pin = settings.getString("pin","");
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                if(pin.equals("")) //if it's a new user
+                {
+                    Intent intent = new Intent(getApplicationContext(), Setpin.class);
+                    startActivity(intent);
+                    finish();
+                }
+
+                else //if pin already set by user
+                {
+                    Intent intent = new Intent(getApplicationContext(), Enterpin.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        }, 2000);
+    }
+}
